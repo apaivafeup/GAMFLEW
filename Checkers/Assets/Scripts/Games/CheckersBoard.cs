cdc21a4663ba7903d346be43636210089edc7663
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-07-11 11:21:15 +0100

    Update CheckersBoard.cs

--- Checkers/Assets/Scripts/Games/CheckersBoard.cs
+++ Checkers/Assets/Scripts/Games/CheckersBoard.cs
@@ -30,6 +30,7 @@ public class CheckersBoard : MonoBehaviour
     {
         isWhiteTurn = true;
         forcedPieces = new List<Piece>();
+        isWhite = true;
         GenerateBoard();
     }
     private void Update()
