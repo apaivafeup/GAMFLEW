88c53f0fe9ce78a81ccea59a0084cacd3eebca51
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-10-23 13:59:06 +0100

    Timer is working.

--- Prototype/WhiteBoxCapsule/src/components/ChallengeHeader.vue
+++ Prototype/WhiteBoxCapsule/src/components/ChallengeHeader.vue
@@ -4,7 +4,8 @@ defineProps({
     type: String,
     required: true
   },
-  timer: Number
+  timer: Number,
+  title: String
 })
 </script>
 
@@ -12,9 +13,30 @@ defineProps({
   <header>
     <div class="row justify-content-between" id="header-row">
       <div class="col" id="challenge-id">Challenge 1</div>
-      <div class="col" id="challenge-timer">{{ timer }}</div>
+      <div class="col" v-if="this.board.timer > 100" id="challenge-timer">{{ this.board.timer }}</div>
+      <div class="col" v-else id="challenge-timer-hurry">{{ this.board.timer }}</div>
     </div>
   </header>
 </template>
+
+<script>
+import { bluePos, redPos, boardStore } from '../store/boardStore'
+
+export default {
+  data() {
+    return {}
+  },
+
+  beforeMount() {
+    this.board = boardStore()
+
+    setInterval(() => {
+      this.board.timer--;
+    }, 1000)
+  },
+
+  methods: {},
+}
+</script>
 
 <style scoped></style>
