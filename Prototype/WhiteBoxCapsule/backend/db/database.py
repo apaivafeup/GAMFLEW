672ba7f1811f1f77ea48a5b38110865acd16df5f
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-12-13 13:59:07 +0000

    Added environment variables to hopefully work with prod database later.

--- Prototype/WhiteBoxCapsule/backend/db/database.py
+++ Prototype/WhiteBoxCapsule/backend/db/database.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv   #for python-dotenv method
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/prototype"
+local = True
+
+load_dotenv()
+
+# Create a connection string
+TECH = 'postgresql' if local else os.environ.get('TECH')
+USERNAME = 'postgres' if local else os.environ.get('USERNAME')
+PASSWORD = 'password' if local else os.environ.get('PASSWORD')
+HOST = 'localhost' if local else os.environ.get('HOST')
+DATABASE = 'prototype' if local else os.environ.get('DATABASE')
+
+SQLALCHEMY_DATABASE_URL = f"{TECH}://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
