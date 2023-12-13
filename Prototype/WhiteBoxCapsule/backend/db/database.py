48b05f1fb013c647ef918bef90f83a229bb59675
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-12-13 18:32:42 +0000

    Update database.py

--- Prototype/WhiteBoxCapsule/backend/db/database.py
+++ Prototype/WhiteBoxCapsule/backend/db/database.py
@@ -1,7 +1,9 @@
+from urllib.parse import quote
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv   #for python-dotenv method
+import os
 
 local = True
 
@@ -14,7 +16,7 @@ PASSWORD = 'password' if local else os.environ.get('PASSWORD')
 HOST = 'localhost' if local else os.environ.get('HOST')
 DATABASE = 'prototype' if local else os.environ.get('DATABASE')
 
-SQLALCHEMY_DATABASE_URL = f"{TECH}://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
+SQLALCHEMY_DATABASE_URL = f"{TECH}://{USERNAME}:%s@{HOST}/{DATABASE}" % quote(PASSWORD)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
