c92d04ec78fe09c7a97a82fc395329341e46d24d
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2023-12-13 11:59:54 +0000

    Update on home page tests.

--- Prototype/WhiteBoxCapsule/frontend/cypress/e2e/home.cy.js
+++ Prototype/WhiteBoxCapsule/frontend/cypress/e2e/home.cy.js
@@ -26,22 +26,4 @@ describe('Home Page', () => {
     cy.contains('When you click the')
 
   })
-
-  it('starts a challenge', () => {
-    cy.visit('http://localhost:5173/')
-    cy.get('#single-player-button').click()
-    cy.get('#challenge-card-' + Math.ceil(Math.random(15))).click()
-    
-    cy.get('#guide-modal')  
-    .should('be.visible')
-    .wait(500)
-    
-    cy.get('#guide-modal-close-button').click().wait(500)
-
-    cy.get('#guide-modal')  
-    .should('not.be.visible')
-
-    cy.get('.button.is-primary.is-fullwidth').should('be.visible')
-
-  })
 })
