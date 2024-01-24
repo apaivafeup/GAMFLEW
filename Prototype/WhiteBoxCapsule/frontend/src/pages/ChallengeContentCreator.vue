41a143797394b6ec809b609698915415e7fc43aa
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-01-24 10:50:47 +0000

    Choosing board state.

--- Prototype/WhiteBoxCapsule/frontend/src/pages/ChallengeContentCreator.vue
+++ Prototype/WhiteBoxCapsule/frontend/src/pages/ChallengeContentCreator.vue
@@ -3,6 +3,7 @@ import GameCredits from './GameCredits.vue'
 import Menu from '../components/Menu.vue'
 import BoardCreator from '../components/BoardCreator.vue'
 import { boardCreatorStore } from '../store/boardCreator'
+import dropdown from 'vue-dropdowns';
 
 export default {
   async beforeMount() {
@@ -12,9 +13,8 @@ export default {
       response.data.forEach((board_state) => {
         this.boardStates.push(board_state)
       })
+      this.dropdownClick(0, true);
     })
-
-     document.getElementById('flexRadioDefault1').checked = true
   },
 
   data() {
@@ -22,7 +22,8 @@ export default {
       board: true,
       details: false,
       code: false,
-      boardStates: []
+      boardStates: [],
+      selectedState: ''
     }
   },
 
@@ -43,13 +44,34 @@ export default {
         this.details = false
         this.code = !this.code
       }
-  
+
       document.getElementById(current + '-button').classList.toggle('selected')
       document.getElementById(state + '-button').classList.toggle('selected')
+    },
+
+    toggleDropdown() {
+      document.getElementById('dropdown-menu-states').classList.toggle('show')
+      document.getElementById('dropdown-menu-states').style.display = (document.getElementById('dropdown-menu-states').style.display == '' ? 'block' : '')
+      document.getElementById('dropdown-menu-states').style.position = 'absolute'
+      document.getElementById('dropdown-menu-states').style.inset = '0px auto auto 0px'
+      document.getElementById('dropdown-menu-states').style.margin = '0px'
+      document.getElementById('dropdown-menu-states').style.transform = 'translate3d(12px, 57.6px, 0px)'
+    },
+
+    dropdownClick(id, firstTime = false) {
+      this.selectedState = this.boardStates[id].name
+
+      if (!firstTime) {
+        this.toggleDropdown()
+      }
+
+      this.board.changeState(this.boardStates[id])
     }
 
   },
-  components: { Menu, BoardCreator }
+  components: {
+    Menu, BoardCreator
+  }
 }
 </script>
 
@@ -58,25 +80,16 @@ export default {
     <h2>Challenge Content Creator</h2>
   </div>
   <div class="row" style="justify-content: center;">
-    <button
-    class="menu-button selected"
-    id="board-button"
-    @click="this.change('board')"
-    style="width: 30%; padding: 5px;"
-  >
-    Board State
-  </button>
-  <button
-    class="menu-button"
-    id="code-button"
-    @click="this.change('code')"
-    style="width: 30%;"
-  >
-    Code
-  </button>
+    <button class="menu-button selected" id="board-button" @click="this.change('board')"
+      style="width: 30%; padding: 5px;">
+      Board State
+    </button>
+    <button class="menu-button" id="code-button" @click="this.change('code')" style="width: 30%;">
+      Code
+    </button>
   </div>
   <div class="row" style="justify-content: center;">
-     <div class="col" style="width: 50%;">
+    <div class="col" style="width: 50%;">
       <div class="row">
         <h3>Board States</h3>
       </div>
@@ -85,28 +98,23 @@ export default {
       </div>
       <div class="row">
         <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown button
+          <div v-if="this.selectedState != ''" style="font-size: 10px;">
+            <p style="margin-bottom: 5px;"><strong>Selected: </strong>{{ this.selectedState }}</p>
+          </div>
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false" @click="toggleDropdown()" style="margin: 0px;">
+            Load State
           </button>
-          <div class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <div id="dropdown-menu-states" class="dropdown-menu">
+            <button class="dropdown-item" type="button" v-for="state in boardStates" v-bind:key="state.id - 1"
+              @click="dropdownClick(state.id - 1)">{{ state.name }}</button>
           </div>
         </div>
       </div>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Load State
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <button class="dropdown-item" type="button" v-for="state in boardStates" v-bind:key="state.id - 1" @change="this.board.changeState(this.boardStates[state.id - 1])">{{state.name}}</button>
-        </div>
-      </div>
 
-      </div>
-      <div class="col" style="width: 50%;">
-       <BoardCreator v-if="board" />
-     </div> 
+    </div>
+    <div class="col" style="width: 50%;">
+      <BoardCreator v-if="board" />
+    </div>
   </div>
 </template>
