abfd20c704055e60142576151ef14d3befd00c3d
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-01-26 11:57:18 +0000

    Add code file viewer.
    Next thing: validation expressions.

--- Prototype/WhiteBoxCapsule/frontend/src/pages/TeacherView.vue
+++ Prototype/WhiteBoxCapsule/frontend/src/pages/TeacherView.vue
@@ -22,6 +22,18 @@ export default {
     await this.$axios.get(this.$api_link + '/board-states').then((response) => {
       response.data.forEach((board_state) => {
         this.boardStates.push(board_state)
+        this.selectedState = 1
+      })
+    })
+
+    await this.$axios.get(this.$api_link + '/code-files').then((response) => {
+      response.data.forEach((codeFile) => {
+        this.codeFiles.push(codeFile)
+        
+        if (codeFile.id == 1) {
+          this.selectedCode = 1
+          this.codeString = codeFile.content
+        }
       })
     })
   },
@@ -29,10 +41,11 @@ export default {
   data() {
     return {
       boardStates: [],
+      codeFiles: [],
       selectedState: '',
       stateName: '',
-      codeName: '',
-      codeString: '// Placeholder code\n\nfunction hello() {\n  console.log("Hello World!");\n}\n\nhello();'
+      selectedCode: '',
+      codeString: ''
     }
   },
 
@@ -54,6 +67,11 @@ export default {
     selectState(id) {
       this.selectedState = id
       this.boardCreator.changeState(this.boardStates[id - 1])
+    },
+
+    selectCode(id) {
+      this.selectedCode = id
+      this.codeString = this.codeFiles[id - 1].content
     }
 
   },
@@ -70,9 +88,38 @@ export default {
     <h2>Teacher View</h2>
   </div>
   <div class="container" style="display: flex; justify-content: center;">
-    <div class="row" style="width: 525px; justify-content: center; display: flex; flex-direction: column;">
+    <div class="row" style="justify-content: center; display: flex; flex-direction: row;">
       <div class="col">
-
+        <div class="row" style="width: 100%; margin: 0px;">
+          <h6 style="padding: 0px; margin-bottom: 5px;">Code File</h6>
+        </div>
+        <div class="row" style="font-size: 10px;">
+          <p style="margin-bottom: 5px;">Choose an existing code file below.</p>
+        </div>
+        <div class="row" style="width: 100%; margin-left: 0px; margin-bottom: 10px;">
+          <select style="padding: 5px; width: 650px;">
+            <option @click="this.selectCode(code.id)" v-for="code in codeFiles" :value="code.id">{{code.name}}</option>
+          </select>
+        </div>
+        <div class="row" style="width: 100%; padding: 0px; margin: 0px;">
+          <CodeBlock
+                id="code-block-example"
+                class="line-numbers"
+                theme="default"
+                height="440px"
+                data-line="1"
+                :prismjs="true"
+                :name="name"
+                :code="codeString"
+                lang="javascript"
+                prism-js
+                style="font-size: 16px; width: 650px;"
+                :copy-icon="false"
+                :copy-button="false"
+                :copy-tab="false"
+                :tabs="false"
+          />
+        </div>
       </div>
       <div class="col">
         <div class="row" style="width: 100%; margin: 0px;">
