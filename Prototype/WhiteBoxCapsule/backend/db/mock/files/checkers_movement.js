90b348832237d3e3613316b4f07b1969f3ffb1a7
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-02-01 14:47:18 +0000

    Organizing.

--- Prototype/WhiteBoxCapsule/backend/db/mock/files/checkers_movement.js
+++ Prototype/WhiteBoxCapsule/backend/db/mock/files/checkers_movement.js
@@ -1,45 +1,5 @@
 import PieceStackCreatorVue from "../../../../frontend/src/components/PieceStackCreator.vue";
 
-// piece is the piece that is moving.
-// start: the (x, y) position from where the piece moved.
-// destination: the (x, y) position to where the piece moved.
-function is_valid_move(piece, start, destination) {
-    // Math.abs() gives you the absolute value of whatever you call it with.
-    // Math.abs(-1) == 1
-	if (destination.x < 0 || destination.x > 7 ||
-        destination.y < 0 || destination.y > 7) {
-        return false;
-    }
-
-    var lineDifference = Math.abs(start.x - destination.x);
-    var columnDifference = Math.abs(start.y - destination.y);
-
-    if (lineDifference != columnDifference) {
-        return false;
-    } else {
-        if (lineDifference == 1) {
-            if (board[destination.x][destination.y].color == Color.EMPTY) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (lineDifference == 2) {
-            if (board[destination.x][destination.y].color == Color.EMPTY) {
-                var middlePiece = board[Math.round((start.x + destination.x) / 2)][Math.round((start.y + destination.y) / 2)];
-                if (middlePiece.color == piece.color) {
-                    return false;
-                } else {
-                    return true;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-}
-
 function is_board_valid(board) {
     var bluePieces = utils.count_blue_pieces(board);
     var redPieces = utils.count_red_pieces(board);
