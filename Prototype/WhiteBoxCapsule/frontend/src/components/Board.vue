11f2be180fe20bb42c71d317c59aca7e700300e7
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-03-08 17:21:35 +0000

    Trying to fix condition/decision.

--- Prototype/WhiteBoxCapsule/frontend/src/components/Board.vue
+++ Prototype/WhiteBoxCapsule/frontend/src/components/Board.vue
@@ -42,8 +42,8 @@ export default {
       <div class="row" style="display: flex; flex-direction: row; width: 100%;">
         <ChallengeCode :code_file="code_file" style="width: 100%;" />
       </div>
-      <div class="row" style="width: 100%;">
-        <div class="alert alert-secondary player-info" style="width: 100%; overflow-y: scroll; font-size: 14px;" id="#challenge-hint" v-if="!board.passed">
+      <div class="row" style="width: 100%;" v-if="!board.passed">
+        <div class="alert alert-secondary player-info" style="width: 100%; overflow-y: scroll; font-size: 14px;" id="#challenge-hint" >
           <p style="margin: 0px; padding: 0px; align-self: center;">
             <b>Hint:</b> {{ this.challenge.hint }}
           </p>
