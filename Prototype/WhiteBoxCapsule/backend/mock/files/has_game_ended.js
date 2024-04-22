4a83d0121376fab7a17b571c6d2eb613fe369e5b
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-04-22 19:38:40 +0100

    New challenges.

--- Prototype/WhiteBoxCapsule/backend/mock/files/has_game_ended.js
+++ Prototype/WhiteBoxCapsule/backend/mock/files/has_game_ended.js
@@ -1,11 +1,10 @@
-// I could make this way more complicated...
 function has_game_ended(board) {
     var pieces = utils.get_pieces(board);
 
     // If there is only one piece left, the game has ended.
     if (pieces.length == 1) {
         return true;
-    } else if (pieces.all(p => p.color == Color.RED) || pieces.all(p => p.color == Color.BLUE)) {
+    } else if (pieces.every(p => p.color == Color.RED) || pieces.every(p => p.color == Color.BLUE)) {
         return true;
     } else {
         return false;
