77c1efec07602df19e2ca42cbf1b065b483e384d
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-04-28 13:00:13 +0100

    Adapting old challenges into new files, because I like them.

---
+++ Prototype/WhiteBoxCapsule/backend/mock/files/build_a_triangle.js
@@ -0,0 +1,18 @@
+function build_a_triangle(board) {
+	var a = this.count_red_pieces(board); // Counts the number of blue pieces.
+	var b = this.count_blue_pieces(board); // Counts the number of red pieces.
+	var c = this.count_empty_spaces(board); // Counts the number of empty spaces (no pieces).
+	
+    // Tests if the lengths of the sides of the triangle are valid.
+	if (this.isTriangle(a, b, c)) {
+		return true;
+	} else {
+		if (b < c && b > a) {
+			return true;
+		} else if (b == a || b == c) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
