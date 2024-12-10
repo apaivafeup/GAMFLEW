ff57c47f5ac13227e6a86547014eba07e0bead8c
lessthelonely <lessthelonely@gmail.com>
2024-12-10 22:23:23 +0000

    Multiplayer cancel pass and modes fixed.

--- Prototype/WhiteBoxCapsule/frontend/src/components/ChallengeHeader.vue
+++ Prototype/WhiteBoxCapsule/frontend/src/components/ChallengeHeader.vue
@@ -6,16 +6,16 @@
         ><em>{{ name.includes(':')?name.split(':')[1] : name}}</em>
       </div>
       <div class="col" v-if="id != null" id="challenge-buttons" style="display: flex; justify-content: end;">
-        <button class="button" style="width: auto !important;" v-if="id != 1" @click="this.$forceUpdate(); this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) - 1).toString() }}); this.$router.go(1)">
+        <button class="button" style="width: auto !important;" v-if="id != 1" @click="changePreviousPage(id, true)">
           Previous Challenge
         </button>
-        <button class="button disabled" style="width: auto !important;"  v-else  @click="this.$forceUpdate(); this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) - 1).toString() }}); this.$forceUpdate();">
+        <button class="button disabled" style="width: auto !important;"  v-else  @click="changePreviousPage(id, false)">
           Previous Challenge
         </button>
-        <button class="button" style="width: auto !important;"  v-if="id != 99" @click="this.$forceUpdate(); this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) + 1).toString() }}); this.$router.go(1)">
+        <button class="button" style="width: auto !important;"  v-if="id != 99" @click="changeNextPage(id, false)">
           Next Challenge
         </button>
-        <button class="button disabled" style="width: auto !important;" v-else @click="this.$forceUpdate(); this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) + 1).toString() }}); this.$forceUpdate();">
+        <button class="button disabled" style="width: auto !important;" v-else @click="changeNextPage(id, true)">
           Next Challenge
         </button>
       </div>
@@ -33,6 +33,43 @@ export default {
     id: Number
   },
 
-  methods: {}
+  beforeMount() {
+    this.board = boardStore()
+  },
+
+
+  methods: {
+    changeAddMode(){
+      if (this.board.add) {
+        this.board.addMode();
+      }
+    },
+
+    changePreviousPage(id, one) {
+      this.changeAddMode();
+      this.$forceUpdate(); 
+      if (one) {
+        this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) - 1).toString() }}); 
+        this.$router.go(1);
+      } else {
+        this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) - 1).toString() }}); 
+        this.$forceUpdate();
+      }
+  },
+
+  changeNextPage(id, last) {
+    this.changeAddMode();
+    this.$forceUpdate(); 
+    if (last) {
+      this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) + 1).toString() }}); 
+      this.$forceUpdate();
+    }
+    else {
+      this.$router.push({ name: 'challenge', params: { id: parseInt(parseInt(id) + 1).toString() }}); 
+      this.$router.go(1);
+    }
+  },
+
+  }
 }
 </script>
