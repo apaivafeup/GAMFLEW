65e46b05f3143b5c539902e5b4296bcf2c77ccbe
lessthelonely <lessthelonely@gmail.com>
2025-06-28 12:16:20 +0100

    Some changes.

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://frontend:80", "http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
