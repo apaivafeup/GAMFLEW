6004fa587ae7c2954b76c79256effbb1b230b20e
lessthelonely <lessthelonely@gmail.com>
2025-07-09 16:26:20 +0100

    FIx.

--- backend/database.py
+++ backend/database.py
@@ -2,19 +2,13 @@ from urllib.parse import quote
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv   #for python-dotenv method
 import os
-
-load_dotenv()
-
-#local = eval(os.environ.get('LOCAL'))
-is_docker = os.path.exists('/.dockerenv') 
 
 # Create a connection string
 TECH = 'postgresql' #if local else os.environ.get('TECH')
 USERNAME = 'postgres' #if local else 'feupptgamflew'
 PASSWORD = 'password' #if local else os.environ.get('PASSWORD')
-HOST = 'host.docker.internal' #if (is_docker and local) else '127.0.0.1' if local else os.environ.get('HOST')
+HOST = 'host.docker.internal' #if local else os.environ.get('HOST')
 DATABASE = 'prototype' #if local else os.environ.get('DATABASE')
 PORT = 5432
 
